45f115b8930ab8cbd64c2327785e2e65f31664c3
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-08 13:21:14 -0600

    added functions to js file

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+
+
 // There should be a function defined with the name checkWordCount.
-// That function should return false if a string sent to it has more than 100 words in it.
+function checkWordCount() {
+    // That function should return false if a string sent to it has more than 100 words in it.
+
+
+};
+
+
 // There should be a function defined with the name duplicateCheck.
-// That function should return false if a string has duplicate words in it.
+
+function duplicateCheck() {
+    // That function should return false if a string has duplicate words in it.
+
+};
+
 // There should be a function defined with the name verifyAlphaNumeric.
-// That function should return false if there are any non-alphanumeric characters in a string.
+function verifyAlphaNumeric(){
+    // That function should return false if there are any non-alphanumeric characters in a string.
+
+
+};
+
+
 // Now that the test suite exists, write a simple application to implement the functionality.
