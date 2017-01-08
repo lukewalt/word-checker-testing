1753c419c58b5c409b8d77aa9d9d854526fde1a7
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-08 13:18:54 -0600

    added requirement comments to js file

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-console.log("hey");
-
-$('body').append("<h1>HEY</h1>");
+// There should be a function defined with the name checkWordCount.
+// That function should return false if a string sent to it has more than 100 words in it.
+// There should be a function defined with the name duplicateCheck.
+// That function should return false if a string has duplicate words in it.
+// There should be a function defined with the name verifyAlphaNumeric.
+// That function should return false if there are any non-alphanumeric characters in a string.
+// Now that the test suite exists, write a simple application to implement the functionality.
